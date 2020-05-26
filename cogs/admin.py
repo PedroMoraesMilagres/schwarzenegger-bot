f1c923d1574e79ad0475d8f2e9ced2394c600f20
PedroMoraesMilagres <pedromoraesmilagres@gmail.com>
2020-05-26 13:30:15 -0300

    retirado os comandos load, reload e unload

--- cogs/admin.py
+++ cogs/admin.py
@@ -47,21 +47,6 @@ class Admin(commands.Cog):
         await ctx.send(f'{member.mention} foi kickado. \nMotivo: {reason}')
         return
 
-    @commands.command(hidden=True)
-    @commands.is_owner()
-    async def load(self, ctx, extension):
-        self.bot.load_extension(f'cogs.{extension}')
-
-    @commands.command(hidden=True)
-    @commands.is_owner()
-    async def unload(self, ctx, extension):
-        self.bot.unload_extension(f'cogs.{extension}')
-
-    @commands.command(hidden=True)
-    @commands.is_owner()
-    async def reload(self, ctx, extension):
-        self.bot.reload_extension(f'cogs.{extension}')
-
 
 def setup(bot):
     bot.add_cog(Admin(bot))
